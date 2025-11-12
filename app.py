9bcb18eb725577952a606d91a2eaf42811fb387c
Stacer15-EA <swhomes15@gmail.com>
2025-11-11 19:36:47 -0800

    My recording keeps lagging

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", subtract(5, 3))
 
-print("Please work I am begging you. I need this to work for my CI/CD demo.")
+print("Please recording, do not make me do this again.")
